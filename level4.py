4834338f23d2a1cee1b248b277b1db51ab519f70
grinyahoo <grinyahoo.f@gmail.com>
2019-04-24 13:37:17 -0400

    level 4 done

---
+++ level4.py
@@ -0,0 +1,41 @@
+# Python Challenge. level 4 Linked list
+# Hint: urllib may help. DON'T TRY ALL NOTHINGS, since it will never end. 400 times is more than enough.
+# Hint: link to "linkedlist.php?nothing=12345". By clicking response "and the next nothing is 44827"
+
+# Hint: at nothing 16044 response 'Yes. Divide by two and keep going.'
+
+import re
+from urllib.request import urlopen
+
+def main():
+
+    # Fetch html
+
+    next_nothing('48827')
+
+    # with urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s" % nothing) as html:
+    #
+    #     nothing = html.find(r'and the next nothing is (/d+)')
+    #     print html
+    #     print nothing
+
+def next_nothing(nothing, counter=400):
+
+    if counter > 0:
+        with urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s" % nothing) as response:
+
+            html = response.read().decode('utf-8')
+            next = re.findall(r'and the next nothing is (\d+)', html)
+            print(html)
+            # print(next)
+
+        if next:
+            if next[0] == '16044':
+                next[0] = '8022'
+
+            next_nothing(next[0], counter - 1)
+
+main()
+
+
+# result peak.html
